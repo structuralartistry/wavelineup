803b78dd342f3ed9d59554c4f583adee2a8e4dbc
structuralartistry <dk@structuralartistry.com>
2012-04-02 19:12:58 -0400

    initial working rails 3.2 install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wavelineup::Application.config.secret_token = '50a38467ecb9bae819e6b358bbf7e033e332ab1e28486eb5e2e6566a40cf926548438bae5870c2eead9241a10d1a9d3220ff71b53e669e3a6a5c8cd5b5272588'
