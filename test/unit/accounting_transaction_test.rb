bf288178e3159e8451c2046b140333a1b8cee0f5
structuralartistry <dk@structuralartistry.com>
2012-05-07 08:38:24 -0700

    fix test

--- test/unit/accounting_transaction_test.rb
+++ test/unit/accounting_transaction_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class AccountingTransactionTest < ActiveSupport::TestCase
   should validate_presence_of :date_time
-  should validate_presence_of :credit_debit_id
+  should validate_presence_of :credit_debit_key
   should validate_presence_of :amount
-  should validate_presence_of :category_id
-  should validate_presence_of :account_id
+  should validate_presence_of :category_key
+  should validate_presence_of :account_key
 end
